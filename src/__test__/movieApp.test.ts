704f8de45e62603729866356c7083f850f7940b8
EZillman <evelina.zillman@medieinstitutet.se>
2023-02-01 21:49:52 +0100

    tried a test- it failed

--- src/__test__/movieApp.test.ts
+++ src/__test__/movieApp.test.ts
@@ -1,3 +1,35 @@
 /**
  * @jest-environment jsdom
  */
+
+import * as movieApp from '../ts/movieApp';
+
+beforeEach(() => {
+    document.body.innerHTML = "";
+});
+
+
+describe('everything with init', () => {
+    test('should call handleSubmit', () => {
+
+        // Arrange
+        document.body.innerHTML = `
+        <form id="searchForm">
+            <input type="text" id="searchText" placeholder="Skriv titel här" />
+            <button type="submit" id="search">Sök</button>
+        </form>
+        `;
+
+        let tiredSpy = jest.spyOn(movieApp, 'handleSubmit').mockReturnValue(new Promise<void>((resolve) => {
+            resolve();
+        }));
+
+        // Act
+        movieApp.init();
+
+        // Assert
+        (document.querySelector('searchForm') as HTMLFormElement)?.submit();
+        expect(tiredSpy).toHaveBeenCalled();
+        tiredSpy.mockRestore();
+    });
+});
