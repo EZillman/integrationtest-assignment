26a9b3521546102779027fc98ac347f698f7629a
EZillman <evelina.zillman@medieinstitutet.se>
2023-02-02 17:32:25 +0100

    test for movieservice.ts

--- src/__test__/movieservice.test.ts
+++ src/__test__/movieservice.test.ts
@@ -1,3 +1,47 @@
 /**
  * @jest-environment jsdom
  */
+
+
+import { getData, testArray } from "../ts/services/__mocks__/movieservice";
+
+
+jest.mock('axios', () => ({
+    get: async (url: string) => {
+        return new Promise((resolve, reject) => {
+            if (url.endsWith('error')) {
+                reject([]);
+            }
+            else {
+                resolve ({data: {Search: testArray}});
+            }
+        });
+
+    }
+}));
+
+
+describe('everything with getData', () => {
+
+    test('should fetch data from testArray', async () => {
+
+        // Arrange
+
+        // Act 
+        let result = await getData();
+    
+        // Assert
+        expect(result.length).toBe(3);
+        expect(result[0].Title).toBe('Bella');
+
+    });
+
+    test('should not be able to fetch data from testArray', async () => {
+        try {
+            await getData();
+        } catch (error: any) {
+            expect(error.length).toBe(0);
+        }
+
+    });
+});
--- src/ts/services/__mocks__/movieservice.ts
+++ src/ts/services/__mocks__/movieservice.ts
@@ -1,9 +1,9 @@
 import { IMovie } from '../../models/Movie';
 
 export let testArray: IMovie[] = [
-    {Title: 'Bella', imdbID: '222', Type: 'Mother', Poster: 'Missing', Year: '1970'},
-    {Title: 'Mortimer', imdbID: '444', Type: 'Father', Poster: 'Home', Year: '1968'},
-    {Title: 'Cassandra', imdbID: '888', Type: 'Daughter', Poster: 'Away', Year: '1990'}
+    {Title: 'Bella', imdbID: '222', Type: 'First', Poster: 'None', Year: '1970'},
+    {Title: 'Ella', imdbID: '444', Type: 'Second', Poster: 'Gone', Year: '1968'},
+    {Title: 'Stella', imdbID: '888', Type: 'Third', Poster: 'Gnome', Year: '1990'}
 ];
 
 export async function getData(): Promise<IMovie[]> {
